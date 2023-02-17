bb7cce5c83dda6d5af573c8a83528ede153b84e1
meatsby <meatsby@gmail.com>
2023-02-17 20:35:53 +0800

    chore: 프로젝트 초기 설정을 세팅한다. (#2)

---
+++ src/test/java/com/cityu/kascu/KascuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cityu.kascu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KascuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
